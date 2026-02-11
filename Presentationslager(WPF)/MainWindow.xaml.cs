cc8f977114eb9fa5afeb95a187d609ee3d0ae01e
Moa Haugeto <s2408778@student.hb.se>
2026-02-11 13:07:23 +0100

    Add project files.

---
+++ Presentationslager(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOSU2_VT26_Grupp_07.Presentationslager_WPF_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void loggaInButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hej! {användarnamnTexbox.Text}");
+        }
+    }
+}
